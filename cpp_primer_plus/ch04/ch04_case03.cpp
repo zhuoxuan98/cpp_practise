7a9f82243b75fe33715c29231a8cf4bf96b0a245
zhuo_xuan <otronic@012ZHUOXUAN>
2023-12-20 20:02:14 +0800

    cpp_primer_plus:第四章01-04

---
+++ cpp_primer_plus/ch04/ch04_case03.cpp
@@ -0,0 +1,21 @@
+//
+// Created by otronic on 2023/12/19.
+//
+#include <iostream>
+
+int main()
+{
+    using namespace  std;
+    const int ArSize = 20 ;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name:\n";
+    cin >> name;
+    cout << "Enter your favorite dessert:\n";
+    cin >> dessert;
+    cout << "I have some delicious" << dessert;
+    cout << " for you, " << name << ".\n";
+    return 0;
+
+}
